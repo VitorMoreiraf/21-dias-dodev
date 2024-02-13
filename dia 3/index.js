abf396638dccbf539c75b991a20985d11bf42cd0
Vitor Moreira <vitormoreira5091@gmail.com>
2024-02-13 10:29:55 -0300

    adicionando dia 3

---
+++ dia 3/index.js
@@ -0,0 +1,19 @@
+let nomeDoUsuario = prompt('Insira seu nome');
+let idadeDoUsuario = parseInt(prompt('Insira sua idade'));
+let anoDeNascimento = parseInt(2023 - idadeDoUsuario);
+let alturaDoUsuario = parseFloat(prompt('Insira sua altura'));
+let peso = prompt('Insira seu peso');
+let pesoDoUsuario = Number(peso);
+
+IMC = pesoDoUsuario / (alturaDoUsuario **2);
+let IMCDoUsuario = IMC.toFixed(2);
+
+console.log(nomeDoUsuario)
+console.log(idadeDoUsuario)
+console.log(alturaDoUsuario)
+console.log(pesoDoUsuario)
+
+console.log(IMCDoUsuario)
+
+console.log('Olá '+ nomeDoUsuario + ', você tem ' + idadeDoUsuario +' anos, nasceu em ' + anoDeNascimento + 
+', tem ' + alturaDoUsuario +' de altura, pesa ' +  pesoDoUsuario + 'kg seu IMC é ' + IMCDoUsuario + ' Kg/m2.');  
